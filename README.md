ff120584f767dfb00f440ee3132a95662047ce91
i-Mhyke <58915377+i-Mhyke@users.noreply.github.com>
2020-07-24 02:46:54 +0100

    Initial commit
    Nodejs Task 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Wejapa-Node-Task-1
-Task to create a node server without a framework
+### Task to create a node server without a framework
+### Run `npm start` in npm console and visit http://localhost:8080 on the browser
--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const http = require('http');
 
 const port = 8080;
 
-http.createServer((req, res)=>{
+const server = http.createServer((req, res)=>{
 	res.writeHead(200);
 	res.end('Hello World, welcome to WeJapa Internships');
-}).listen(port, 'localhost', ()=>{
+})
+server.listen(port, ()=>{
 	console.log(`Server is running on http://localhost:${port}`)
 });
